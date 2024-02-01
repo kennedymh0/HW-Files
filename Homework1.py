c188ac7172799d9d40c9887291c2582ecacea4da
Kennedy Haynes <kmhaynes@berkeley.edu>
2024-02-01 11:50:49 -0800

    replaced homework file with vs code homework file

--- Homework1.py
+++ Homework1.py
@@ -1,1 +1,4 @@
-
+print (10+((2-35)*307)+3829)
+# I truthfully did not put much thought into the math problem, I just typed numbers and functions.
+# I also chose the name "Homework1" because this is the first homework assignment😍
+# I ran this in Terminal by "python3 Homework1"
