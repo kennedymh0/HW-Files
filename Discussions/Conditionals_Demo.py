5244c8aa73f878ffeac2082ba5d54c6892e226ae
Kennedy Haynes <kmhaynes@berkeley.edu>
2024-02-09 13:52:49 -0800

    added discussions folder

---
+++ Discussions/Conditionals_Demo.py
@@ -0,0 +1,23 @@
+# compare x with 10
+x = 4
+if (x > 10):
+    print("x is greater than 10")
+elif (x == 10):
+    print("x is 10")
+else:
+    print("x is less than 10")
+
+    # while loop
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1 # i = i - 1
+print('this is a test')
+for i in range(1, 11):
+    print(i)
+print('did this work')
+# sum the number 1-10
+count = 0
+for i in range(1, 11):
+    count += i
+print(count)
